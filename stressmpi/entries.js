ee826a177c1e59fdf7f5ee760870d9e8c57d9abc
François C <79398531+Effece@users.noreply.github.com>
2026-09-22 23:37:09 +0200

    Update entries.js

--- stressmpi/entries.js
+++ stressmpi/entries.js
@@ -1,32 +1,32 @@
 const infos = {
 	"ecritsX": {
 		label: "Ecrits (X/ENS)",
-		func: countdown("Apr 13, 2026 08:00:00"),
+		func: countdown("Apr 13, 2027 08:00:00"),
 		class: "c0"
 	},
 	"ecritsXse": {
 		label: "Ecrits en semaines (X/ENS)",
-		func: countdownw("Apr 13, 2026 08:00:00"),
+		func: countdownw("Apr 13, 2027 08:00:00"),
 		class: ""
 	},
 	"ecritsMines": {
 		label: "Ecrits (Mines-Ponts)",
-		func: countdown("Apr 27, 2026 08:00:00"),
+		func: countdown("Apr 27, 2027 08:00:00"),
 		class: "c1"
 	},
 	"fin": {
 		label: "Fin de la prepa",
-		func: countdown("Aug 1, 2026 08:00:00"),
+		func: countdown("Jul 24, 2027 08:00:00"),
 		class: ""
 	},
 	"pourcentageEcoule": {
 		label: "Pourcentage d'avancement",
-		func: pourcentage("Sep 1, 2025 08:00:00", "Apr 13, 2026 08:00:00"),
+		func: pourcentage("Sep 1, 2026 08:00:00", "Apr 13, 2027 08:00:00"),
 		class: "c1"
 	},
 	"pourcentageTotal": {
 		label: "Pourcentage d'avancement (sup incluse)",
-		func: pourcentage("Sep 1, 2024 10:00:00", "Apr 13, 2026 08:00:00"),
+		func: pourcentage("Sep 1, 2025 10:00:00", "Apr 13, 2027 08:00:00"),
 		class: ""
 	},
 	"cours": {
@@ -40,7 +40,7 @@ const infos = {
 	},
 	"semainesRestant": {
 		label: "Semaines totales",
-		func: semscours("Apr 13, 2026 08:00:00"),
+		func: semscours("Apr 13, 2027 08:00:00"),
 		class: ""
 	},
 	"heures": {
@@ -49,27 +49,27 @@ const infos = {
 	},
 	"mathsRestant": {
 		label: "Maths",
-		func: heures("m", "Apr 13, 2026 08:00:00"),
+		func: heures("m", "Apr 13, 2027 08:00:00"),
 		class: ""
 	},
 	"phyRestant": {
 		label: "Physique",
-		func: heures("p", "Apr 13, 2026 08:00:00"),
+		func: heures("p", "Apr 13, 2027 08:00:00"),
 		class: ""
 	},
 	"infoRestant": {
 		label: "Informatique",
-		func: heures("i", "Apr 13, 2026 08:00:00"),
+		func: heures("i", "Apr 13, 2027 08:00:00"),
 		class: ""
 	},
 	"frRestant": {
 		label: "Français",
-		func: heures("f", "Apr 13, 2026 08:00:00"),
+		func: heures("f", "Apr 13, 2027 08:00:00"),
 		class: ""
 	},
 	"angRestant": {
 		label: "Anglais",
-		func: heures("a", "Apr 13, 2026 08:00:00"),
+		func: heures("a", "Apr 13, 2027 08:00:00"),
 		class: ""
 	},
 	"ds": {
@@ -136,7 +136,7 @@ const infos = {
 	},
 	"5/2": {
 		label: "Temps avant l'éventuel début de 5/2",
-		func: countdown("Sep 1, 2026 08:00:00"),
+		func: countdown("Sep 1, 2027 08:00:00"),
 		class: ""
 	}
 
